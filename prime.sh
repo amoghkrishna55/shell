9fac1c12365b2ead41b810c22bf1e735ebbedec4
Amogh Krishna <amoghkrishna55@gmail.com>
2023-08-23 07:49:57 +0530

    prime number fixed

--- prime.sh
+++ prime.sh
@@ -1,28 +1,16 @@
 echo "Enter a number: "
 read num
-num1=1
-if [ $num -le 2 ]
-then 
-	echo "Not a prime"
-else 
-	while [ $num1 -le $num ]
-	do
-		num2=1
-		a=1
-		while [ $num2 -le $num1 ]
-		do
-			if [ `expr $num2 % 2` -eq 0 ]
-			then
-				a=0
-				break
-			fi
-			if [ $a -eq 1 ]
-			then
-				echo $num2
-			fi
-			num2=`expr $num2 + 1`
-		done
-		num1=`expr $num1 + 1`
-	done
-fi
 
+echo "Prime numbers up to $num:"
+for ((i = 2; i <= num; i++)); do
+    is_prime=1
+    for ((j = 2; j * j <= i; j++)); do
+        if [ $((i % j)) -eq 0 ]; then
+            is_prime=0
+            break
+        fi
+    done
+    if [ $is_prime -eq 1 ]; then
+        echo $i
+    fi
+done
