a5763ddb87e6f0443d25d1b6c67b16d74a1c9c88
Amogh Linux <amoghkrishna55@icloud.com>
2023-09-06 15:57:38 +0530

    Fixed half_half.c

--- half_half.c
+++ half_half.c
@@ -7,14 +7,16 @@
 
 int main(){
 	int fd1,fd2,fd3;
-	char buf[10];
+	char buf[100];
 	fd1=open("sample.txt",O_RDONLY,0777);
 	fd2=open("half1.txt",O_WRONLY,0777);
 	fd3=open("half2.txt",O_WRONLY,0777);
-	read(fd1,buf,10);
-	write(fd2,buf,10);
-	read(fd1,buf,10);
-	write(fd3,buf,10);
+	int l = lseek(fd1,0,SEEK_END);
+	lseek(fd1,0,SEEK_SET);
+	read(fd1,buf,l/2);
+	write(fd2,buf,l/2);
+	read(fd1,buf,l/2);
+	write(fd3,buf,l/2);
 	close(fd1);
 	close(fd2);
 	close(fd3);
