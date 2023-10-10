ddc0d3b6473f4d6049fed3c48b28ba8fa217d27f
Amogh Krishna <amoghkrishna55@gmail.com>
2023-10-11 00:18:05 +0530

    thread1

--- thread1.c
+++ thread1.c
@@ -1,24 +1,25 @@
-#include<stdio.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h> 
 
-void* conc(void* arr, void* ans){
-	char* myarr, myans;
-	myarr = (char*) arr;
-	myans = (char*) ans;
-	strcopy(arr,ans);
-	return ans;
+void* conc(void* arr, void* ans) {
+    char* myarr = (char*)arr;
+    char* myans = (char*)ans; 
+    strcpy(myans, myarr);
+    return NULL;
 }
 
-
-int main(){
-	char* line, ans="";
-	int a;
-	pthread_t thread1;
-	scanf("%d", &a);
-	while(a--){
-		scanf("%c",line);
-		pthread_create(&thread1, NULL, conc, (void*)line);
-		pthread_join(thread1, &ans);
-	}
-	printf("%c",ans)
+int main() {
+    char line[100];
+    char ans[100] = "";
+    int a;
+    pthread_t thread1;
+    scanf("%d", &a);
+    while (a--) {
+        scanf("%s", line);
+        pthread_create(&thread1, NULL, conc, (void*)line);
+        pthread_join(thread1, NULL);
+    }
+    printf("%s\n", ans);
+    return 0;
 }
