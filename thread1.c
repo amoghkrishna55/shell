470057a17d7dfc135f63a68b613579cac8201cdd
Amogh Krishna <amoghkrishna55@gmail.com>
2023-10-10 23:56:21 +0530

    thread1

--- thread1.c
+++ thread1.c
@@ -1,23 +1,40 @@
-#include<stdio.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
 
-void *func(){
-	printf("In the thread function \n");
-	pthread_exit("Exit thread function \n");
-}
-int main() {
-    char *strings[] = {"Hello, ", "World! ", "This ", "is ", "a ", "single-threaded ", "concatenation ", "example."};
-    int num_strings = sizeof(strings) / sizeof(strings[0]);
+#define MAX_STRINGS 10
+#define MAX_STRING_LENGTH 100
 
-    // Calculate the total length of the concatenated string
+void *concatenate_strings(void *arg) {
+    char *strings[MAX_STRINGS];
+    int num_strings = *(int *)arg;
     int total_length = 0;
     for (int i = 0; i < num_strings; i++) {
+        strings[i] = ((char **)arg)[i + 1];
         total_length += strlen(strings[i]);
     }
-
-    // Allocate memory for the concatenated string
     char *concatenated_string = (char *)malloc(total_length + 1);
-    if (concatenated_string == NULL) {
-        perror("Memory allocation error");
+    int index = 0;
+    for (int i = 0; i < num_strings; i++) {
+        strcpy(&concatenated_string[index], strings[i]);
+        index += strlen(strings[i]);
+    }
+    pthread_exit(concatenated_string);
+}
+
+int main() {
+    char *strings[] = {"Hello, ", "World! ", "This ", "is ", "a ", "multi-threaded ", "concatenation ", "example."};
+    int num_strings = sizeof(strings) / sizeof(strings[0]);
+    pthread_t thread;
+    if (pthread_create(&thread, NULL, concatenate_strings, (void *)&num_strings) != 0) {
+        perror("Thread creation error");
         return 1;
     }
+    char *concatenated_string;
+    if (pthread_join(thread, (void **)&concatenated_string) != 0) {
+        perror("Thread join error");
+        return 1;
+    }
+    printf("%s\n", concatenated_string);
+}
