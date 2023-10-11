f0c71f3a5ca645c0a80ffa55865ded1bddb5ce8c
Amogh Linux <amoghkrishna55@icloud.com>
2023-10-11 15:36:35 +0530

    CA

--- threadCA2.C
+++ threadCA2.C
@@ -23,9 +23,8 @@ int main() {
     pthread_t thread1, thread2;
     
     pthread_create(&thread1, NULL, even, NULL);
-    pthread_create(&thread2, NULL, odd, NULL);
-    
     pthread_join(thread1, NULL);
+    pthread_create(&thread2, NULL, odd, NULL);
     pthread_join(thread2, NULL);
 
     return 0;
